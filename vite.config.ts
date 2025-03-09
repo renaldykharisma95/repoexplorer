d0708f07dd7170cc56223c8a4dc54b0c88b3336f
renaldykharisma95 <renaldykhrsm@outlook.com>
2025-03-09 07:42:12 +0700

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-  base: "/githubrepoexplorer/",
+  base: "/repoexplorer/",
   test: {
     globals: true,
     environment: "jsdom",
